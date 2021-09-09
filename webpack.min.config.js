f6ca118d934dd81bedf9274948b0e2012f7a316a
deplax <sturdyegg@gmail.com>
2021-09-09 21:58:41 +0900

    미디어쿼리 수정 && 날짜 추가 && 버튼 정리

--- webpack.min.config.js
+++ webpack.min.config.js
@@ -35,6 +35,7 @@ module.exports = {
         new CopyPlugin({
             patterns: [
                 { from: "src/img", to: "img" },
+                { from: "src/css", to: "css" },
             ],
         }),
     ],
